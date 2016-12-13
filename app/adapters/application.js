e085c757992e3ebdb0b6373cec74d35fb9d6311c
TimoWintz <tim.wintz@gmail.com>
2016-12-13 15:36:03 +0100

    stuff

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-    host: 'http://localhost:8337',
     pathForType: function(type) { return Ember.String.underscore(type); },
     urlForQuery(query, modelName) {
-        var url = this.host + '/' + modelName + '/query/' + query;
+        var url = modelName + '/query/' + query;
         return url;
     }
 });
