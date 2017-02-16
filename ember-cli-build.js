ab991d176e148c123118c0e5fd2095a061295630
TimoWintz <tim.wintz@gmail.com>
2017-02-16 11:29:01 +0100

    added import jplayer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,6 +21,8 @@ module.exports = function(defaults) {
     // modules that you would like to import into your application
     // please specify an object with the list of modules as keys
     // along with the exports of each module as its value.
+    //   app.import("bower_components/jquerypic/js/jquerypic.js");
+	app.import("bower_components/jplayer/dist/jplayer/jquery.jplayer.js");
 
     return app.toTree();
 };
